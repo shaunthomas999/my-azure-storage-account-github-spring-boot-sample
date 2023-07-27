3744866e6f2787e88aa4640057e28eb7bdcef7e8
Shaun Thomas <Shaun.Thomas@klm.com>
2023-07-27 11:02:38 +0200

    first commit

---
+++ src/main/java/io/shaunthomas/storage/MyAzureStorageAccountGithubSpringBootSampleApplication.java
@@ -0,0 +1,13 @@
+package io.shaunthomas.storage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyAzureStorageAccountGithubSpringBootSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyAzureStorageAccountGithubSpringBootSampleApplication.class, args);
+    }
+
+}
